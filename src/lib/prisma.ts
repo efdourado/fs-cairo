662dc9a1059ff325bb1dd63ffeedcc5748dacb59
Eduardo Dourado <ed320819@gmail.com>
2025-08-16 09:45:16 -0300

    code cleanup and minor fixes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global { var cachedPrisma: PrismaClient; }
-
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export const db = prisma;
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
